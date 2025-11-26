52225c2e6e0be317ffe0464be3eb28e68db60e05
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2025-11-26 21:41:07 +0700

    first dag

---
+++ dags/test_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.amazon.aws.transfers.sftp_to_s3 import SFTPToS3Operator
+from airflow.operators.email import EmailOperator
+from datetime import timedelta
+
+default_args = {
+    "owner": "data-team",
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="daily_sftp_ingest_log",
+    start_date=days_ago(1),
+    schedule_interval="0 2 * * *",   
+    default_args=default_args,
+    catchup=True,     
+    max_active_runs=1 
+) as dag:
+
+    ingest_log = SFTPToS3Operator(
+        task_id="ingest_log_file",
+        sftp_conn_id="web-server-log",       
+        aws_conn_id="aws_default",    
+       
+        sftp_path="/home/dev/logs/ingest_date={{ ds }}/*.ndjson",
+        s3_bucket="lakehouse",
+        s3_key="bronze/user_activity_logs/ingest_date={{ ds }}/{{ filename }}",
+        replace=True,     # ghi đè nếu tồn tại
+        use_temp_file=True
+    )
+    
+    ingest_log
