71fcc2a3a6cfaf81840f2846586ffec0e89f18f0
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2025-12-29 03:30:41 +0700

    aaa

--- dags/user_activity_logs_pipeline.py
+++ dags/user_activity_logs_pipeline.py
@@ -153,6 +153,7 @@ with DAG(
         task_id="dbt_compile",
         bash_command=f"cd {DBT_PROJECT_DIR} && dbt compile  "
                      f"--vars '{{\"etl_date\": \"{{{{ ds }}}}\", "
+                            f"\"etl_year\": \"{{{{ execution_date.strftime('%Y') }}}}\", "
                             f"\"etl_month\": \"{{{{ execution_date.strftime('%m') }}}}\"}}'",
         env={
             "DBT_PROFILES_DIR": DBT_PROJECT_DIR,
@@ -172,6 +173,7 @@ with DAG(
                 task_id=f"run_{model_name}",
                 bash_command=f"cd {DBT_PROJECT_DIR} && dbt run --select {model_name}"
                             f"--vars '{{\"etl_date\": \"{{{{ ds }}}}\", "
+                            f"\"etl_year\": \"{{{{ execution_date.strftime('%Y') }}}}\", "
                             f"\"etl_month\": \"{{{{ execution_date.strftime('%m') }}}}\"}}'",
                 env={
                     "DBT_PROFILES_DIR": DBT_PROJECT_DIR,
@@ -198,6 +200,7 @@ with DAG(
                 task_id=f"run_{model_name}",
                 bash_command=f"cd {DBT_PROJECT_DIR} && dbt run --select {model_name}"
                             f"--vars '{{\"etl_date\": \"{{{{ ds }}}}\", "
+                            f"\"etl_year\": \"{{{{ execution_date.strftime('%Y') }}}}\", "
                             f"\"etl_month\": \"{{{{ execution_date.strftime('%m') }}}}\"}}'",
                 env={
                     "DBT_PROFILES_DIR": DBT_PROJECT_DIR,
